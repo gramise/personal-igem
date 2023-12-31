8ee85d6ccf70f1f0ba1798dbcd7a6139ac6c892e
gramise <139209581+gramise@users.noreply.github.com>
2023-12-31 22:47:34 +0800

    Add files via upload

---
+++ igem个人作业/static/js/color.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      backgroundColor: theme => ({
+       ...theme('colors'),
+       'primary': '#3490dc',
+       'secondary': '#ffed4a',
+       'danger': '#e3342f',
+      })
+    }
+  }
